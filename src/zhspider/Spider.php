b7d29eec52fec7d9968f23f877900579d78fc012
fsyd <fsyd@vip.com>
2017-06-23 17:35:49 +0800

    Signed-off-by: fsyd <fsyd@vip.com>

--- src/zhspider/Spider.php
+++ src/zhspider/Spider.php
@@ -14,6 +14,8 @@ class Spider {
 
     //run
     public function run() {
+        ini_set('max_execution_time', '0');
+        error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
         $engine = new Engine();
         $engine->run();
     }
--- src/zhspider/handle/DyttHandle.php
+++ src/zhspider/handle/DyttHandle.php
@@ -42,22 +42,51 @@ class DyttHandle extends BaseHandle {
 
     //获取需要的信息
     protected function getInfo($xpath) {
-        $nodeList = $xpath->query('//*[@id="Zoom"]//p[1]');
+        echo 'h-';
+        $nodeList = $xpath->query('//*[@id="Zoom"]');
         if ($nodeList->length == 0) {
-            $this->log('没获取到相关的数据 xpath->//*[@id="Zoom"]//p[1]');
+            $this->log('没获取到相关的数据 xpath->//*[@id="Zoom"]');
             return false;
         }
+
         if ($nodeList->length > 0) {
             $str = $nodeList->item(0)->nodeValue;
+            file_put_contents('b.txt', $str);
             //解析数据
-            file_put_contents('a.txt', $str);
-        }
+            $data = $this->parseDy($str);
+            //获取图片
+            $nodeList = $xpath->query('//*[@id="Zoom"]//img');
+            foreach ($nodeList as $v) {
+                $tmp[] = $v->getAttribute('src');
+            }
+            $data['pics'] = json_encode($tmp, JSON_UNESCAPED_UNICODE);
+            unset($tmp);
 
-        echo "h-";
+            if ($nodeList->item(0)) {
+                $data['pic_title'] = $nodeList->item(0)->getAttribute('src');
+            }
+
+            if ($nodeList->item(1)) {
+                $data['pic_content'] = $nodeList->item(1)->getAttribute('src');
+            }
+            //获取下载地址
+            $nodeList = $xpath->query('//*[@id="Zoom"]/span/table/tbody/tr/td/a');
+            foreach ($nodeList as $v) {
+                $tmp[] = $v->nodeValue;
+            }
+            $data['downurls'] = json_encode($tmp, JSON_UNESCAPED_UNICODE);
+
+            file_put_contents('a.txt', json_encode($data, JSON_UNESCAPED_UNICODE) . "\n", FILE_APPEND);
+        }
+    }
+
+    protected function parse1() {
+        
     }
 
     //解析movie数据
     protected function parseDy($str) {
+        $newArr = array();
         $replace = array(
             'ym' => '译　　名',
             'pm' => '片　　名',
@@ -83,10 +112,10 @@ class DyttHandle extends BaseHandle {
             $keys[] = $k . '#@#';
             $values[] = $v;
         }
+        //去除头尾多余字
+        $str = preg_replace(array('/.*?@/im', '/【下载地址】.*/', '/\r\n/'), '', $str);
         $str = str_replace($values, $keys, $str);
         $arr = explode('◎', $str);
-        array_shift($arr);
-        $newArr = array();
         foreach ($arr as $v) {
             $tmp = explode('#@#', $v);
             $newArr[$tmp[0]] = trim($tmp[1], '\t\n\r\0\x0B\x20　');
@@ -97,37 +126,6 @@ class DyttHandle extends BaseHandle {
     //解析dsj数据
     protected function parseDsj() {
         
-    }
-
-    /**
-     * 把str中的key转换成value
-     * @param type $str
-     * @param type $key
-     */
-    protected function convStr($str, $replace) {
-        $replace = array(
-            'ym' => '译　　名',
-            'pm' => '片　　名',
-            'nd' => '年　　代',
-            'cd' => '产　　地',
-            'lb' => '类　　别',
-            'yy' => '语　　言',
-            'zm' => '字　　幕',
-            'syrq' => '上映日期',
-            'dbpf' => '豆瓣评分',
-            'wjgs' => '文件格式',
-            'spcc' => '视频尺寸',
-            'wjcd' => '文件大小',
-            'pc' => '片　　长',
-            'dy' => '导　　演',
-            'zy' => '主　　演',
-            'jj' => '简　　介',
-            'hjqk' => '获奖情况',
-        );
-        return str_replace(array_values($replace), array_keys($replace), $str);
-//        foreach($replace as $k=>$v){
-//            
-//        }
     }
 
 }
